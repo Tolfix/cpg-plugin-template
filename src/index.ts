6075b3db51329c2b96d721d8fe89b7e42a3ec866
Matthew <matteus@tolfx.dev>
2022-03-14 22:08:18 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,8 @@ import config from "../config.json";
 // Change name of the class.
 export = async function main()
 {
-    const Logger = await import(`${BuildDir}/lib/Logger`) as ILoggingTypes;
-    const MainEvent = await import(`${BuildDir}/Events/Main.event`) as typeof mainEvent;
+    const Logger = (await import(`${BuildDir}/Lib/Logger`)).default as ILoggingTypes;
+    const MainEvent = (await import(`${BuildDir}/Events/Main.event`)).default as typeof mainEvent;
     const Server = await import(`${BuildDir}/Server`) as typeof server;
     const CustomerModel = await import(`${BuildDir}/Database/Models/Customer/Customer.model`) as typeof cs;
     Logger.info(`Starting ${config.name} plugin with version ${config.version}.`);
